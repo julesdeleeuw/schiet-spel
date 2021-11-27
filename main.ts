4cceb923a9ebc007e00b5222330d2eff6d572b2c
julesdeleeuw <91489930+julesdeleeuw@users.noreply.github.com>
2021-11-27 18:25:45 +0100

    pxt.json, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     SHIP.move(-1)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     SHOOT = game.createSprite(SHIP.get(LedSpriteProperty.X), SHIP.get(LedSpriteProperty.Y))
     SHOOT.change(LedSpriteProperty.Brightness, 80)
     for (let index = 0; index < 4; index++) {
@@ -13,32 +14,37 @@ input.onButtonPressed(Button.AB, function () {
             SHOOT.delete()
             ENEMY.delete()
         }
+        
     }
     if (SHOOT.get(LedSpriteProperty.Y) <= 0) {
         SHOOT.delete()
     }
+    
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     SHIP.move(1)
 })
-let ENEMY: game.LedSprite = null
-let SHOOT: game.LedSprite = null
-let SHIP: game.LedSprite = null
+let ENEMY : game.LedSprite = null
+let SHOOT : game.LedSprite = null
+let SHIP : game.LedSprite = null
 SHIP = game.createSprite(2, 4)
 game.setScore(0)
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     ENEMY = game.createSprite(randint(0, 4), 0)
     ENEMY.set(LedSpriteProperty.Brightness, 150)
     basic.pause(100)
     ENEMY.turn(Direction.Right, 90)
-    for (let index = 0; index < 4; index++) {
+    for (let index2 = 0; index2 < 4; index2++) {
         ENEMY.move(1)
         basic.pause(100)
         if (ENEMY.isTouching(SHIP)) {
             game.gameOver()
         }
+        
     }
     if (ENEMY.isTouchingEdge()) {
         ENEMY.delete()
     }
+    
 })
